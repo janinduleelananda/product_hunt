f6836954f1f5e5bd9df7eac00bb56003e5456023
janinduleelananda <janindur@gmail.com>
2020-05-28 00:54:44 +0530

    final

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+admin.site.register(Product)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
